dd949cc85479411c9efba1a533bf12d53ec179a4
Santak Dalai <santakdalai90@live.com>
2023-07-31 08:42:13 +0530

    added primary constructor

--- OOP/src/main/kotlin/Main.kt
+++ OOP/src/main/kotlin/Main.kt
@@ -1,33 +1,19 @@
 fun main(args: Array<String>) {
-    val car1 = Car()
-    car1.name = "Tesla"
-    car1.model = "CyberTruck"
-    car1.color = "Marsh Green"
-    car1.doors = 5
-
+    val car1 = Car("Tesla" , "CyberTruck", "Marsh Green",5)
     println("Car Name = ${car1.name}")
     car1.move()
     car1.stop()
 
     println("----------------------------")
 
-    val car2 = Car()
-    car2.name =  "Tata"
-    car2.model = "Nexon"
-    car2.color = "Blue"
-    car2.doors = 5
+    val car2 = Car("Tata", "Nexon","Blue",5)
 
     println("Car Name = ${car2.name}")
     car2.move()
     car2.stop()
 }
 
-class Car {
-    var name = ""
-    var model = ""
-    var color = ""
-    var doors = 0
-
+class Car (var name: String, var model: String, var color: String, var doors: Int){
     fun move() {
         println("The car $name is moving")
     }
