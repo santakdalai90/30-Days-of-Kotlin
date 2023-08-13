50c7a5d32c61a5a303839a11bc9e9c51ca769fba
Santak Dalai <santakdalai90@live.com>
2023-08-13 12:17:16 +0530

    added example of zipping in collections

--- CollectionsOperations/src/main/kotlin/Main.kt
+++ CollectionsOperations/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 fun main(args: Array<String>) {
+    // mapping
     val numbers = setOf(53, 65, 35, 98, 564, 231, 98)
     println(numbers.map { it * 10 })
     println(numbers.map { if (it % 2 == 0) it * 2 else it * 3 })
@@ -7,4 +8,15 @@ fun main(args: Array<String>) {
     val numMap = mapOf("Archie" to 23, "Frank" to 32, "Sumi" to 30, "Ranu" to 28)
     println(numMap.mapKeys { it.key.uppercase() })
     println(numMap.mapValues { it.value + 2 })
+
+    // zipping
+    val colors = listOf("red", "brown", "blue")
+    val animals = listOf("fox", "lion", "cat")
+    println(colors.zip(animals))
+    println(colors zip animals)
+    println(colors.zip(animals) { color, animal -> "The ${animal.replaceFirstChar { it.uppercase() }} is of $color color" })
+
+    val numPairs = listOf("one" to 1, "thirty" to 30, "Ninety Nine" to 99)
+    println(numPairs)
+    println(numPairs.unzip())
 }
